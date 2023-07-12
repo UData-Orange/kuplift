6d2d71ae7aac401ef70b114b628e3b4ca8753bff
voisine nicolas <93999116+n-voisine@users.noreply.github.com>
2023-07-12 18:38:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kuplift",
-    version="0.0.1",
+    version="0.0.2",
     packages=["kuplift"],
     install_requires=["numpy", "pandas", "sortedcontainers", "scikit-learn", "pytest"],
 )
