58161a568f36152bbcac720b31f980bc6d430495
Mina Rafla <mina.wagdi63@gmail.com>
2023-07-13 10:17:44 +0200

    Update FeatureSelection.py

--- kuplift/FeatureSelection.py
+++ kuplift/FeatureSelection.py
@@ -7,7 +7,7 @@
 # * Unauthorized copying of this file, via any medium is strictly prohibited.        #
 # * See the "LICENSE.md" file for more details.                                      #
 ######################################################################################
-import sys
+import multiprocessing as mp
 from .HelperFunctions import preprocess_data
 from .UMODL_SearchAlgorithm import execute_greedy_search_and_post_opt
 
@@ -64,8 +64,47 @@ class FeatureSelection:
             k: v for k, v in sorted(var_vs_importance.items(), key=lambda item: item[1])
         }
         return var_vs_importance
+    
+    @staticmethod
+    def get_the_best_var_parallel(args):
+        """
+        Parameters
+        ----------
+        data : pd.Dataframe
+            Dataframe containing data.
+        treatment_col : pd.Series
+            Treatment column.
+        y_col : pd.Series
+            Outcome column.
 
-    def filter(self, data, treatment_col, y_col):
+        Returns
+        -------
+        dict
+            A Python dictionary containing the sorted variable importance,
+            where the keys represent the variable names and the values denote
+            their respective importance.
+
+        For example: return a dictionary
+                    var_vs_importance={"age":2.2,"job":2.3}
+        """
+        data, treatment_col, y_col=args[0],args[1],args[2]
+        
+        features = list(data.columns)
+        feature=features[0]
+        features.remove(treatment_col)
+        features.remove(y_col)
+        print("feature is ",feature)
+        var_vs_importance = {}
+        var_vs_disc = {}
+        (
+            var_vs_importance[feature],
+            var_vs_disc[feature],
+        ) = execute_greedy_search_and_post_opt(
+            data[[feature, treatment_col, y_col]]
+        )
+        return (feature,var_vs_importance[feature])
+
+    def filter(self, data, treatment_col, y_col,parallelized=False,num_processes=5):
         """
         This function runs the feature selection algorithm 'UMODL-FS',
         ranking variables based on their importance in the given data.
@@ -84,19 +123,31 @@ class FeatureSelection:
         Python Dictionary
             Variables names and their corresponding importance value (Sorted).
         """
-        stdout_origin = sys.stdout
-        sys.stdout = open("log.txt", "w")
-
         cols = list(data.columns)
 
         cols.remove(treatment_col)
         cols.remove(y_col)
         data = data[cols + [treatment_col, y_col]]
         data = preprocess_data(data, treatment_col, y_col)
+        
+        if parallelized==True:
+            pool = mp.Pool(processes=num_processes)
+            
+            arguments_to_pass_in_parallel=[]
+            for col in cols:
+                arguments_to_pass_in_parallel.append([data[[col,treatment_col,y_col]],treatment_col,y_col])
+            list_of_tuples_feature_vs_importance = pool.map(FeatureSelection.get_the_best_var_parallel, arguments_to_pass_in_parallel)
+            pool.close()
+            
+            #transform tuple to dict
+            list_of_tuples_feature_vs_importance = dict(list_of_tuples_feature_vs_importance)
+            
+            list_of_vars_importance = {
+            k: v for k, v in sorted(list_of_tuples_feature_vs_importance.items(), key=lambda item: item[1])
+            }
+            
+        else:
+            list_of_vars_importance = self.__get_the_best_var(data, treatment_col, y_col)
 
-        list_of_vars_importance = self.__get_the_best_var(data, treatment_col, y_col)
-
-        sys.stdout.close()
-        sys.stdout = stdout_origin
 
         return list_of_vars_importance
