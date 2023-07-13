31e078647aef0a5929dc402e36adafba34c61a8e
PaulineRollier <129269510+PaulineRollier@users.noreply.github.com>
2023-07-13 17:24:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     packages=["kuplift"],
     description="A User Parameter-free Bayesian Framework for Uplift Modeling",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         "numpy",
         "pandas",
