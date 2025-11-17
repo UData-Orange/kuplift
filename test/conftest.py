13ac3ae90e5570d3ee801550c812063d0399867c
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-11-17 10:40:22 +0100

    Add new test data file with categorical variable and missing category

--- test/conftest.py
+++ test/conftest.py
@@ -6,3 +6,7 @@ import pytest
 def test_dataframe():
     df = pd.read_csv("data/zenodo_sample.csv")
     return df
+
+@pytest.fixture()
+def test_dataframe_with_categorical_variable():
+    return pd.read_csv("data/data_uplift_missing.csv")
