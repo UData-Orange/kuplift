95d8e121c1b71598a738c1cefad030569d4f4204
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-12-18 19:09:18 +0100

    Bump version -> 0.0.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "kuplift"
 copyright = "2025, Orange"
 author = "Orange"
-release = "0.0.6"
+release = "0.0.7"
 
 # Be strict about any broken references
 nitpicky = True
