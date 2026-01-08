966cd755c58799d0227fb3ea42c63a1e8ef94cf6
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-01-08 21:10:40 +0100

    Split Python code example into multiple steps to allow interrupting it in inspection mode

--- examples/optimized_univariate_encoding.py
+++ examples/optimized_univariate_encoding.py
@@ -4,18 +4,23 @@ Executing this code will generate a model from the data file and print various
 statistics.
 """
 
-import pandas as pd
-import kuplift as kp
-df = pd.read_csv("data/data_uplift_missing.csv").astype({"VAR2": object, "CIBLE": object})
-data = df[df.columns[:-2]]
-treatment_col = df["TRAITEMENT"]
-target_col = df["CIBLE"]
-ue = kp.OptimizedUnivariateEncoding()
-ue.fit(data, treatment_col, target_col)
-print("\nModel generation complete.")
-input("\nPress enter to display the variable levels...")
-print(ue.get_levels())
-input("\nPress enter to display the target probabilities for variable 'VAR1'...")
-print(ue.get_target_probabilities("VAR1"))
-input("\nPress enter to display the uplift for variable 'VAR1'...")
-print(ue.get_uplift(1, "T0", "VAR1"))
+import pandas
+import kuplift
+
+def main():
+    global ue
+    ue = kuplift.OptimizedUnivariateEncoding()
+    df = pandas.read_csv("data/data_uplift_missing.csv").astype({"VAR2": object, "CIBLE": object})
+    ue.fit(df[df.columns[:-2]], df["TRAITEMENT"], df["CIBLE"])
+    print("\nModel generation complete. OptimizedUnivariateEncoding instance available under the name 'ue'.")
+    print("At any of the following steps, press Enter to continue or type 'stop' then Enter to stop at a given step (useful in inspection mode).")
+    if input("\nPress Enter to display the variable levels...") == "stop": return
+    print(ue.get_levels())
+    if input("\nPress Enter to display the target probabilities for variable 'VAR1'...") == "stop": return
+    print(ue.get_target_probabilities("VAR1"))
+    if input("\nPress Enter to display the uplift for variable 'VAR1'...") == "stop": return
+    print(ue.get_uplift(1, "T0", "VAR1"))
+
+
+if __name__ == "__main__":
+    main()
