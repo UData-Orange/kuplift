9733c2f27bb9c532774f22b395f055a606e0efad
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-04-21 23:18:04 +0200

    Change example so that it shows both 2-treatment case and 3+-treatment case

--- examples/multi_treatment_decision_tree.py
+++ examples/multi_treatment_decision_tree.py
@@ -2,15 +2,17 @@ import logging
 from kuplift import MultiTreatmentDecisionTree
 import pandas
 
-LOGLEVEL = logging.INFO
-LOGLEVEL = logging.DEBUG
-DATASET_PATH = "./data/dataset.csv"
-TREATMENT_NAME = "TRAITEMENT"
-TARGET_NAME = "CIBLE"
-
 if __name__ == "__main__":
-    logging.basicConfig(level=LOGLEVEL)
-    dataset = pandas.read_csv(DATASET_PATH)
+    logging.basicConfig(level=logging.INFO)
+
+    # 2 treatments
+    dataset = pandas.read_csv("./data/data_uplift_missing.csv")
     tree = MultiTreatmentDecisionTree()
-    tree.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    tree.fit(dataset[dataset.columns[:-2]], dataset["TRAITEMENT"], dataset["CIBLE"])
+    print(tree.predict(dataset[dataset.columns[:-2]]))
+
+    # 4 treatments
+    dataset = pandas.read_csv("./data/dataset.csv")
+    tree = MultiTreatmentDecisionTree()
+    tree.fit(dataset[dataset.columns[:-2]], dataset["TRAITEMENT"], dataset["CIBLE"])
     print(tree.predict(dataset[dataset.columns[:-2]]))
