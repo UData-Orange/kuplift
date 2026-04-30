a3a5b71d622c4d1f33638669de1e0d6dfeb49edb
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-04-30 11:21:43 +0200

    Update example

--- examples/multi_treatment_decision_tree.py
+++ examples/multi_treatment_decision_tree.py
@@ -2,17 +2,24 @@ import logging
 from kuplift import MultiTreatmentDecisionTree
 import pandas
 
+LOGLEVEL = logging.INFO
+LOGLEVEL = logging.DEBUG
+
+TREATMENT_NAME = "TRAITEMENT"
+TARGET_NAME = "CIBLE"
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=LOGLEVEL)
 
-    # 2 treatments
-    dataset = pandas.read_csv("./data/data_uplift_missing.csv")
+    # # 2 treatments
+    # dataset = pandas.read_csv("./data/data_uplift_missing.csv").astype({"VAR2": object, TREATMENT_NAME: object, TARGET_NAME: object})
+    dataset = pandas.read_csv("./data/data_uplift_missing.csv").astype({TREATMENT_NAME: object, TARGET_NAME: object})
     tree = MultiTreatmentDecisionTree()
-    tree.fit(dataset[dataset.columns[:-2]], dataset["TRAITEMENT"], dataset["CIBLE"])
-    print(tree.predict(dataset[dataset.columns[:-2]]))
+    tree.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # print(tree.predict(dataset[dataset.columns[:-2]]))
 
-    # 4 treatments
-    dataset = pandas.read_csv("./data/dataset.csv")
-    tree = MultiTreatmentDecisionTree()
-    tree.fit(dataset[dataset.columns[:-2]], dataset["TRAITEMENT"], dataset["CIBLE"])
-    print(tree.predict(dataset[dataset.columns[:-2]]))
+    # # 4 treatments
+    # dataset = pandas.read_csv("./data/dataset.csv")
+    # tree = MultiTreatmentDecisionTree()
+    # tree.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # print(tree.predict(dataset[dataset.columns[:-2]]))
