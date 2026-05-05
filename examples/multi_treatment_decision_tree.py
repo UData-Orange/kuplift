918a5de5b929bcd6475e9d859ecec3ddf6e9766a
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-05-05 10:58:46 +0200

    Update decision tree example

--- examples/multi_treatment_decision_tree.py
+++ examples/multi_treatment_decision_tree.py
@@ -9,22 +9,26 @@ LOGLEVEL = logging.DEBUG
 
 TREATMENT_NAME = "TRAITEMENT"
 TARGET_NAME = "CIBLE"
+CATEGORICAL_VAR_NAME = "VAR2"
 
 if __name__ == "__main__":
     logging.basicConfig(level=LOGLEVEL)
 
     # # 2 treatments
-    dataset = pandas.read_csv("./data/data_uplift_missing.csv").astype({"VAR2": object, TREATMENT_NAME: object, TARGET_NAME: object})
+    dataset = pandas.read_csv("./data/data_uplift_missing.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
     tree = MultiTreatmentDecisionTreeV3()
     tree.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # print(tree.tree_to_mermaid())
     tree.print_tree(show_path=True)
-    print()
-    print()
     print(tree.predict(dataset[dataset.columns[:-2]]))
 
+    print()
+    print()
+
     # 4 treatments
-    dataset = pandas.read_csv("./data/dataset.csv").astype({"VAR2": object, TREATMENT_NAME: object, TARGET_NAME: object})
+    dataset = pandas.read_csv("./data/dataset.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
     tree = MultiTreatmentDecisionTreeV3()
     tree.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # print(tree.tree_to_mermaid())
     tree.print_tree(show_path=True)
     print(tree.predict(dataset[dataset.columns[:-2]]))
