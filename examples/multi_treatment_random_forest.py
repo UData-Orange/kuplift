0a621fdba93fbe3d86b51f95cdbee5ee358c5321
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-05-12 11:20:00 +0200

    Clean-up example

--- examples/multi_treatment_random_forest.py
+++ examples/multi_treatment_random_forest.py
@@ -20,14 +20,11 @@ if __name__ == "__main__":
     forest.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
     print(forest.predict(pandas.DataFrame([[0.2, 0.4, 0.2, 0.3], [0.6, 0.8, 0.1, 0.1]], columns=["VAR1", "VAR2", "VAR3", "VAR4"])))
 
-    # print()
-    # print()
-
-    # # 4 treatments
-    # dataset = pandas.read_csv("./data/dataset.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
-    # forest = RandomForest(n_trees=5, max_depth=5)
-    # forest.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
-
+    # 4 treatments
+    dataset = pandas.read_csv("./data/dataset.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
+    forest = RandomForest(n_trees=5, max_depth=5, control_name="T0")
+    forest.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    print(forest.predict(pandas.DataFrame([[0.2, "B", 0.4, 0.2, 0.3], [0.6, "A", 0.8, 0.1, 0.1]], columns=["VAR1", "VAR2", "VAR3", "VAR4", "VAR5"])))
 
 
     # dataset = pandas.read_csv("./data/synthetic_10k.txt", sep="\t").astype({"T": object, "Y": object})
