d66e9655d10739002a6f70da973519c0d4fa6300
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2026-05-19 11:27:48 +0200

    Configure random forest properly

--- examples/multi_treatment_random_forest.py
+++ examples/multi_treatment_random_forest.py
@@ -18,14 +18,26 @@ if __name__ == "__main__":
 
     # 2 treatments
     dataset = pandas.read_csv("./data/data_uplift_missing.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
-    forest = MultiTreatmentRandomForest(n_trees=5, max_depth=5, control_name="T0")
+    forest = MultiTreatmentRandomForest(
+        n_trees=80,
+        max_features=3,          # sous-espace par arbre
+        split_max_features=2,    # sous-espace à chaque split
+        max_depth=6,
+        min_samples_leaf=30,
+        random_state=42,
+        control_name="T0"
+        )
     forest.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # for i, tree in enumerate(forest.trees):
+    #     tree.tree_to_image(f"images/T2__tree{i+1}.png")
     print(forest.predict(pandas.DataFrame([[0.2, 0.4, 0.2, 0.3], [0.6, 0.8, 0.1, 0.1]], columns=["VAR1", "VAR2", "VAR3", "VAR4"])))
 
     # 4 treatments
     dataset = pandas.read_csv("./data/dataset.csv").astype({CATEGORICAL_VAR_NAME: object, TREATMENT_NAME: object, TARGET_NAME: object})
     forest = MultiTreatmentRandomForest(n_trees=5, max_depth=5, control_name="T0", max_cores=2)
     forest.fit(dataset[dataset.columns[:-2]], dataset[TREATMENT_NAME], dataset[TARGET_NAME])
+    # for i, tree in enumerate(forest.trees):
+    #     tree.tree_to_image(f"images/Tn__tree{i+1}.png")
     print(forest.predict(pandas.DataFrame([[0.2, "B", 0.4, 0.2, 0.3], [0.6, "A", 0.8, 0.1, 0.1]], columns=["VAR1", "VAR2", "VAR3", "VAR4", "VAR5"])))
 
 
